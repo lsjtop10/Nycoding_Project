71c735ee4249fa0c9fb63b6df9d31d4cdaf2762d
기주 이 <bob2762@naver.com>
2020-12-26 15:59:47 +0900

    미션

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -7,7 +7,8 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             //Console.WriteLine("hello + 자기 이름 써서 푸시")
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello !");
+            Console.WriteLine("hello 이기주");
         }
     }
 }
